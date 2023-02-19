87eb745090f0fc80fd53aa72ab2636b3f9990b37
Tatevik8 <tvk25@bk.ru>
2023-02-19 13:08:29 +0300

    User insert number and program write third digit (correct version)

---
+++ domzad2correct/Program.cs
@@ -0,0 +1,16 @@
+﻿// **Задача 13:** Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+if (number!.Length < 3)
+{
+    System.Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    System.Console.WriteLine(number[2]);
+}
